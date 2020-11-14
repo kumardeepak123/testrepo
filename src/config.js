feadc51c89a39c69f296f582635a707e45698b53
DEEPAK KUMAR SAHOO <56642683+kumardeepak123@users.noreply.github.com>
2020-11-14 13:22:19 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const tokenUrl =
+    'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/82aae304-d489-4f9d-8ce3-0bc5302ae11a/token';
+const instanceLocator = 'v1:us1:82aae304-d489-4f9d-8ce3-0bc5302ae11a';
+const key =
+    'b20fd3b5-2ead-4923-b055-099ec8f67a0e:5GgSeEtznm/GJB5woZthyhZpHgcZ6e6FGye8UfSs7vg=';
+
+export { tokenUrl, instanceLocator, key };
